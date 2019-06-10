e33f0d0d48848ed108a4c1d4feb3134d0b27eeaf
Ruilova <rruilova2@utmachala.edu.ec>
2019-06-09 20:45:57 -0500

    Codigo 2 actualizado a una suma

--- codigo_2.java
+++ codigo_2.java
@@ -1,7 +1,11 @@
 public class codigo_2 {
 
      public static void main(String[] args) {
-         a = 1
-         System.out.println("numero = "+a);  	
+         int a = 1;
+         int b = 2;
+         int c = a + b;
+         System.out.println("numero a = "+a);
+         System.out.println("numero b = "+b);
+  	 System.out.println("suma = "+c);
      }
 }
