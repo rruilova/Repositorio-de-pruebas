e4d18b6f337be088c106c633f8b571379fc9e399
Ruilova <rruilova2@utmachala.edu.ec>
2019-06-09 21:49:55 -0500

    Codigo 2 actualizado v2.0

---
+++ codigo_3.java
@@ -0,0 +1,11 @@
+public class codigo_3 {
+
+     public static void main(String[] args) {
+         int a = 1;
+         int b = 2;
+         int c = a + b;
+         System.out.println("numero a = "+a);
+         System.out.println("numero b = "+b);
+         System.out.println("suma = "+c);
+     }
+}
