7997f06fdeafb856a0359a7fd213e37edcf28389
Ruilova <rruilova2@utmachala.edu.ec>
2019-06-09 21:58:28 -0500

    Codigo 3 actualizado

--- codigo_3.java
+++ codigo_3.java
@@ -3,7 +3,7 @@ public class codigo_3 {
      public static void main(String[] args) {
          int a = 1;
          int b = 2;
-         int c = a + b;
+         int c = a - b;
          System.out.println("numero a = "+a);
          System.out.println("numero b = "+b);
          System.out.println("suma = "+c);
